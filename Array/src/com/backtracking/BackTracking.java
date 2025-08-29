80ae413a4ac28b4c4fda266eb34bdda25d00a166
S K Priyanshu <sk.priyanshukeshri@gmail.com>
2025-08-30 01:24:20 +0530

    Back Tracking Basics

---
+++ Array/src/com/backtracking/BackTracking.java
@@ -0,0 +1,25 @@
+package com.backtracking;
+
+public class BackTracking {
+    public  static void changeArr(int[] arr , int i , int val){
+        //base case
+        if(i == arr.length){
+            printArr(arr);
+            return;
+        }
+        arr[i] = val;
+        changeArr(arr , i+1 , val+1); //function call
+        arr[i] = arr[i] -2;//backing tracking
+    }
+    public static void printArr(int[] arr){
+        for (int num : arr){
+            System.out.print(num+" ");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        int[] arr = new int[5];
+        changeArr(arr , 0 ,1);
+        printArr(arr);
+    }
+}
