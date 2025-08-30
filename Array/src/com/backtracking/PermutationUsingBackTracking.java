4d7283ad7eb3c9761b02982823866488eb6c61bd
S K Priyanshu <sk.priyanshukeshri@gmail.com>
2025-08-30 12:40:48 +0530

    permutation using Back tracking

---
+++ Array/src/com/backtracking/PermutationUsingBackTracking.java
@@ -0,0 +1,26 @@
+package com.backtracking;
+
+public class PermutationUsingBackTracking {
+    public static void findPermutation(String str , String ans){
+        // Base case
+        if(str.length() == 0){
+            System.out.println(ans);
+            return;
+        }
+
+        // recursion
+        for (int i = 0 ; i<str.length() ;i++){
+            char currentch  = str.charAt(i);
+            //"abcd" => "ab" +"d"
+            String newStr = str.substring(0, i)+ str.substring(i+1 );
+            findPermutation(newStr , ans+currentch);
+        }
+    }
+
+    public static void main(String[] args) {
+        String str = "abc";
+        String ans ="";
+        findPermutation(str , ans);
+    }
+
+}
